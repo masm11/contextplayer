651840a701304ac73136a22d448bb8b62813051a
Yuuki Harano <masm@masm11.ddo.jp>
2016-10-23 10:24:52 +0900

    再生できない曲を skip するようにした。

--- app/src/main/java/jp/ddo/masm11/cplayer/PlayerService.java
+++ app/src/main/java/jp/ddo/masm11/cplayer/PlayerService.java
@@ -9,6 +9,7 @@ import android.os.IBinder;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class PlayerService extends Service {
     private String topDir;
@@ -60,32 +61,82 @@ public class PlayerService extends Service {
 		curPlayer.release();
 		curPlayer = null;
 	    }
+	} catch (Exception e) {
+	    android.util.Log.e("cplayer", "exception", e);
+	}
+	try {
 	    if (nextPlayer != null) {
 		nextPlayer.release();
 		nextPlayer = null;
 	    }
-	    
-	    curPlayer = MediaPlayer.create(this, Uri.parse("file://" + playingPath));
-	    curPlayer.start();
 	} catch (Exception e) {
 	    android.util.Log.e("cplayer", "exception", e);
+	}
+	
+	HashSet<String> tested = new HashSet<>();
+	while (true) {
+	    try {
+		if (tested.contains(playingPath)) {
+		    // 再生できるものがない…
+		    break;
+		}
+		tested.add(playingPath);
+		
+		curPlayer = MediaPlayer.create(this, Uri.parse("file://" + playingPath));
+		if (curPlayer == null) {
+		    android.util.Log.w("PlayerService", "MediaPlayer.create() failed: " + playingPath);
+		    playingPath = selectNext(playingPath);
+		    continue;
+		}
+		curPlayer.start();
+		break;
+	    } catch (Exception e) {
+		android.util.Log.e("cplayer", "exception", e);
+	    }
 	}
     }
     
     private void enqueueNext() {
-	nextPath = selectNext();
-	android.util.Log.i("PlayerService", "nextPath=" + nextPath);
-	nextPlayer = MediaPlayer.create(this, Uri.parse("file://" + nextPath));
-	curPlayer.setNextMediaPlayer(nextPlayer);
+	try {
+	    if (nextPlayer != null) {
+		nextPlayer.release();
+		nextPlayer = null;
+	    }
+	} catch (Exception e) {
+	    android.util.Log.e("cplayer", "exception", e);
+	}
+	
+	HashSet<String> tested = new HashSet<>();
+	nextPath = selectNext(playingPath);
+	while (true) {
+	    try {
+		if (tested.contains(nextPath)) {
+		    // 再生できるものがない…
+		    break;
+		}
+		tested.add(nextPath);
+		
+		nextPlayer = MediaPlayer.create(this, Uri.parse("file://" + nextPath));
+		if (nextPlayer == null) {
+		    android.util.Log.w("PlayerService", "MediaPlayer.create() failed: " + nextPath);
+		    nextPath = selectNext(nextPath);
+		    continue;
+		}
+		curPlayer.setNextMediaPlayer(nextPlayer);
+		break;
+	    } catch (Exception e) {
+		android.util.Log.e("cplayer", "exception", e);
+	    }
+	}
     }
     
-    private String selectNext() {
+    private String selectNext(String nextOf) {
 	// fixme: もちっと効率良く。
 	ArrayList<String> list = new ArrayList<>();
 	scan(new File(topDir), list);
 	Collections.sort(list);
 	for (String path: list) {
-	    if (path.compareTo(playingPath) > 0)
+	    if (path.compareTo(nextOf) > 0)
 		return path;
 	}
 	try {
