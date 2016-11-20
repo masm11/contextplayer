b59b74ef23a63cf8725a0310fd911733b31a1685
Yuuki Harano <masm@masm11.ddo.jp>
2016-11-20 10:40:26 +0900

    wired headset を外した時にも停止するようにした。

---
+++ app/src/main/java/jp/ddo/masm11/contextplayer/HeadsetReceiver.java
@@ -0,0 +1,47 @@
+/* Context Player - Audio Player with Contexts
+    Copyright (C) 2016 Yuuki Harano
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package jp.ddo.masm11.contextplayer;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.Context;
+import android.media.AudioManager;
+
+public class HeadsetReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+	Log.d("");
+	if (intent != null) {
+	    String action = intent.getAction();
+	    Log.d("action=%s", action);
+	    if (action.equals(AudioManager.ACTION_HEADSET_PLUG)) {
+		int state = intent.getIntExtra("state", -1);
+		String name = intent.getStringExtra("name");
+		int microphone = intent.getIntExtra("microphone", -1);
+		Log.d("state=%d", state);
+		Log.d("name=%s", name);
+		Log.d("microphone=%d", microphone);
+		if (state == 0) {
+		    Log.d("headset unplugged.");
+		    Intent i = new Intent(context, PlayerService.class);
+		    i.setAction(PlayerService.ACTION_HEADSET_UNPLUGGED);
+		    context.startService(i);
+		}
+	    }
+	}
+    }
+}
