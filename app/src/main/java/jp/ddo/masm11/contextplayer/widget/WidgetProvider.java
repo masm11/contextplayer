504dbbd8a7af6a9a689705be4c06235d9a2a3ff5
Yuuki Harano <masm@masm11.ddo.jp>
2016-11-26 11:54:05 +0900

    WidgetProvider が何を受信しているのか、log に出力するようにした。

--- app/src/main/java/jp/ddo/masm11/contextplayer/widget/WidgetProvider.java
+++ app/src/main/java/jp/ddo/masm11/contextplayer/widget/WidgetProvider.java
@@ -22,6 +22,7 @@ import android.widget.RemoteViews;
 import android.content.Context;
 import android.content.Intent;
 import android.app.PendingIntent;
+import android.os.Bundle;
 
 import jp.ddo.masm11.contextplayer.R;
 import jp.ddo.masm11.contextplayer.ui.ContextActivity;
@@ -31,6 +32,9 @@ public class WidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 	android.util.Log.d("WidgetProvider", "onUpdate(): begin.");
+	android.util.Log.d("WidgetProvider", "onUpdate(): appWidgetId.length=" + appWidgetIds.length);
+	for (int i = 0; i < appWidgetIds.length; i++)
+	    android.util.Log.d("WidgetProvider", "onUpdate(): appWidgetId=" + appWidgetIds[i]);
 	
 	for (int i = 0; i < appWidgetIds.length; i++) {
 	    int appWidgetId = appWidgetIds[i];
@@ -55,5 +59,22 @@ public class WidgetProvider extends AppWidgetProvider {
 	context.startService(intent);
 	
 	android.util.Log.d("WidgetProvider", "onUpdate(): end.");
+    }
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+	android.util.Log.d("WidgetProvider", "intent=" + intent.toString());
+	String action = intent.getAction();
+	android.util.Log.d("WidgetProvider", "action=" + (action != null ? action : "null"));
+	Bundle bundle = intent.getExtras();
+	if (bundle != null) {
+	    for (String key: bundle.keySet()) {
+		Object val = bundle.get(key);
+		android.util.Log.d("WidgetProvider", "key=" + key + ", val=" + (val != null ? val : "null"));
+	    }
+	} else
+	    android.util.Log.d("WidgetProvider", "extras=none");
+	
+	super.onReceive(context, intent);
     }
 }
