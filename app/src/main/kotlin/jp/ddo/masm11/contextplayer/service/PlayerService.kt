41b3f8d790b709f5ca5e38bf8514e12518155d25
Yuuki Harano <masm+github@masm11.me>
2019-05-19 20:23:32 +0900

    Revert "playingPath を反映。"
    This reverts commit e0b4ab3c3f838936f3cdc67bfb63423ec6939601.

--- app/src/main/kotlin/jp/ddo/masm11/contextplayer/service/PlayerService.kt
+++ app/src/main/kotlin/jp/ddo/masm11/contextplayer/service/PlayerService.kt
@@ -71,7 +71,7 @@ class PlayerService : Service() {
     private fun buildCurrentStatus(): CurrentStatus {
 	return CurrentStatus(
 		contextId,
-		player.playingPath,
+		playingPath,
 		topDir,
 		player.currentPosition.toInt(),
 		player.duration,
